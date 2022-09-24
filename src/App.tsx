4c9b94f159a4c9d02727e8e8928156122a613c47
Belov, Mikhail <mikhailbelov@x5.ru>
2022-09-24 14:51:19 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button} from 'antd'
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>test</h1>
+            <Button></Button>
+        </div>
+    );
+}
+
+export default App;
